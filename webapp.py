09c1a42d8c69875f3bfc43ab853697ec7b21ab12
AvailableUsername124 <kaylebhanacek@gmail.com>
2025-10-24 12:44:19 -0700

    Feat: clean index page

--- webapp.py
+++ webapp.py
@@ -13,7 +13,11 @@ def render_main():
 
 @app.route("/p1")
 def render_page1():
-    return render_template('page1.html')
+    
+    State_Fragility_Indexs = fragility_index()
+    countries = get_country_options()
+    years = get_year_options()
+    return render_template('page1.html', country_options=countries, year_options=years)
 
 @app.route("/p2")
 def render_page2():
@@ -21,25 +25,55 @@ def render_page2():
 
 @app.route('/showFragility')
 def render_fact():
-    states = get_state_options()
-    state = request.args.get('country')
+    countries = get_country_options()
+    country = request.args.get('country','')
+    years = get_year_options()
+    year = request.args.get('year', '')
+    State_Fragility_Indexs = fragility_index()
+    State_Fragility_Index = request.args.get('State Fragility Index', '')
     #county = county_most_under_18(state)
-    county1 = white(state)
-    fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
-    fact1 = "in" + state + ", the country with the lowest percentage of white people is " + county1 + "."
-    return render_template('home.html', state_options=states, funFact=fact, funFact1 = fact1)
+    #county1 = white(state)
+    Fragile1 = "In " + country + ", " + "The fragility is " + year + "."
+    Fragile2 = "In " + country + ", " "the fragility is " + year + "."
+    return render_template('page1.html', country_options=countries, Fragility1=Fragile1, Fragility2 = Fragile2)
 
-def get_state_options():
-    with open('state_fragility.json') as geographics_data:
-        country = json.load(geographics_data)
-    country=[]
-    for c in country:
-        if c["country"] not in country:
-            country.append(c["country"])
+def get_country_options():
+    with open('state_fragility.json') as fragility_data:
+        years = json.load(fragility_data)
+    countries=[]
+    for c in years:
+        if c["Country"] not in countries:
+            countries.append(c["Country"])
     options=""
-    for s in country:
+    for s in countries:
         options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
 
+def get_year_options():
+    with open('state_fragility.json') as fragility_data:
+        countries = json.load(fragility_data)
+    years=[]
+    for c in countries:
+        if c["Year"] not in years:
+            years.append(c["Year"])
+    options=""
+    for s in years:
+        options += Markup("<option value=\"" + str(s) + "\">" + str(s) + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+    return options
+
+def fragility_index():
+    with open('state_fragility.json') as fragility_data:
+        countries = json.load(fragility_data)
+    State_Fragility_Indexs=[]
+    for c in countries:
+        if c["Metrics"]["State Fragility Index"] not in State_Fragility_Indexs:
+            State_Fragility_Indexs.append(c["Metrics"]["State Fragility Index"])
+    options=""
+    for s in State_Fragility_Indexs:
+        options += Markup("<option value=\"" + str(s) + "\">" + str(s) + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+    return options
+
+
+
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
