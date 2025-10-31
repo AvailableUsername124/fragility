6ec91eec9e127f815079608473b059ee182e7ff0
AvailableUsername124 <kaylebhanacek@gmail.com>
2025-10-31 13:33:43 -0700

    Feat: add page 2

--- webapp.py
+++ webapp.py
@@ -13,8 +13,8 @@ def render_main():
 
 @app.route("/p1")
 def render_page1():
-    country = request.args.get('country') #juan
-    year = request.args.get('year') #jaun
+    country = request.args.get('country')
+    year = request.args.get('year')
     State_Fragility_Indexs = fragility_index(country, year)
     countries = get_country_options()
     years = get_year_options()
@@ -22,7 +22,12 @@ def render_page1():
 
 @app.route("/p2")
 def render_page2():
-    return render_template('page2.html')
+    country = request.args.get('country')
+    year = request.args.get('year')
+    effectiveness = effectiveness_index(country, year)
+    countries = get_country_options()
+    years = get_year_options()
+    return render_template('page2.html', country_options=countries, year_options=years)
 
 @app.route('/showFragility')
 def render_fact():
@@ -31,8 +36,23 @@ def render_fact():
     years = get_year_options()
     year = request.args.get('year')
     State_Fragility_Indexs = fragility_index(country, year)
+    effectiveness = effectiveness_index(country, year)
     Fragile1 = "In " + str(country) + ", " + "in the year " + str(year) + " The fragility is " + str(State_Fragility_Indexs) + "." #used ai to add str cause I was actin like a dingleberry and forgot how to fix it ai: https://www.perplexity.ai/search/i-got-the-error-can-only-conca-lwcYFVcxQXKBZE9JvUG90w 
-    return render_template('page1.html', year_options=years, country_options=countries, Fragility1=Fragile1)
+    Fragile2 = "In " + str(country) + ", " + "in the year " + str(year) + " The effectiveness of the government us is " + str(effectiveness) + "."
+    return render_template('page1.html', year_options=years, country_options=countries, Fragility1=Fragile1, Fragility2=Fragile2)
+    
+    
+@app.route('/showEffectiveness')
+def render_fact1():
+    countries = get_country_options()
+    country = request.args.get('country')
+    years = get_year_options()
+    year = request.args.get('year')
+    State_Fragility_Indexs = fragility_index(country, year)
+    effectiveness = effectiveness_index(country, year)
+    Fragile1 = "In " + str(country) + ", " + "in the year " + str(year) + " The fragility is " + str(State_Fragility_Indexs) + "." #used ai to add str cause I was actin like a dingleberry and forgot how to fix it ai: https://www.perplexity.ai/search/i-got-the-error-can-only-conca-lwcYFVcxQXKBZE9JvUG90w 
+    Fragile2 = "In " + str(country) + ", " + "in the year " + str(year) + " The effectiveness of the government us is " + str(effectiveness) + "."
+    return render_template('page2.html', year_options=years, country_options=countries, Fragility1=Fragile1, Fragility2=Fragile2)
 
 def get_country_options():
     with open('state_fragility.json') as fragility_data:
@@ -65,11 +85,17 @@ def fragility_index(country, year):
     for c in countries: 
         if c["Country"] == country and str(c["Year"]) == str(year):
             return c["Metrics"]["State Fragility Index"]
-            #State_Fragility_Indexs.append(c["Metrics"]["State Fragility Index"])
     return None
     
-
-
+def effectiveness_index(country, year):
+    with open('state_fragility.json') as fragility_data:
+        countries = json.load(fragility_data)
+    metric = ""
+    for c in countries: 
+        if c["Country"] == country and str(c["Year"]) == str(year):
+            return c["Metrics"]["Effectiveness"]["Effectiveness Score"]
+    return None
+    
 
 if __name__=="__main__":
     app.run(debug=True)
